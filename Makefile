1c5b9f7e6692f7f2ddd49316d8a95a275abbab3a
Cagri Coltekin <c.coltekin@rug.nl>
2012-08-20 01:22:08 +0200

    xfst/lexc rewrite
    This is a complete xfst/lexc rewrite of SFST sources. The
    intention is to drop the SFST version in time, and only
    maintain this branch. For now, there is nothing exciting,
    the analyzer should be almost the same as the current SFST
    version on the master branch. However, there are some
    differences (which may be bad), and this version is not
    tested and documented as good as the SFST version. On the
    other hand, some wrinkles are ironed out, and there are
    already some gems in place which may be useful for future
    improvements.
    
    This version compiles fine with foma. It should probably
    work fine with HFST too, but it is not yet tested.

---
+++ Makefile
@@ -0,0 +1,13 @@
+LEXICON ?= old
+ROOTLEX=$(shell ls lexicon/$(LEXICON)/*.lexc)
+
+all: trmorph.fst
+
+trmorph.lexc: morph.lexc $(ROOTLEX)
+		cat $^ > $@
+
+trmorph.fst: trmorph.xfst trmorph.lexc
+	foma -f trmorph.xfst
+
+clean:
+	rm -f trmorph.fst trmorph.lexc
---
+++ test/make-expect.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+
+cat << EOF
+#!/usr/bin/expect
+#
+
+set timeout 2
+set CTRLd \004
+log_user 0
+spawn foma
+send ""
+expect -re "foma.0.: "
+send "load stack ../trmorph.fst\n"
+expect -re "foma.1.: "
+send "up\n"
+expect "apply up> "
+
+EOF
+
+
+
+state="analyze"
+sstring="apply up"
+failstring='\?\?\?'
+
+t=$1
+echo 'puts "Testing patterns from '$t' ..."'
+echo 'puts ""'
+OIFS=$IFS
+IFS='
+'
+for line in `sort $t | sed 's/#.*//;/^$/d'`;do
+IFS=$IFS
+    AG=`echo $line|cut -d: -f1`
+    AR=`echo $line|cut -d: -f2`
+    SND=`echo $line|cut -d: -f3`
+    EXP=`echo $line|cut -d: -f4`
+    if [[ -z "$EXP" ]]; then
+        EXP="$failstring"
+        AR="A"
+    fi
+echo '# [dbg] line: ' $line
+echo '# [dbg] snd: ' $SND
+echo '# [dbg] exp: ' $EXP
+
+    if [ $state = "analyze" -a $AG = "G" ]; then
+        echo 'send $CTRLd'
+        echo 'expect -re "foma.1.: "'
+        echo 'send "down\n"'
+        echo 'expect  "apply down> "'
+        echo
+        state="generate"
+        sstring="apply down"
+    elif [ $state = "generate" -a $AG = "A" ]; then
+        echo 'send $CTRLd'
+        echo 'expect -re "foma.1.: "'
+        echo 'send "up\n"'
+        echo 'expect "apply up> "'
+        echo
+        state="analyze"
+        sstring="apply up"
+    fi
+
+    if [[ $AR == "A" ]]; then 
+        if [[ "$EXP" == "$failstring" ]]; then
+            echo 'puts -nonewline "'$state' '$SND' reject ... "'
+        else
+            echo 'puts -nonewline   "'$state' '$SND' accept '$EXP'... "'
+        fi
+        echo 'set found false'
+        echo 'send    "'$SND'\n"'
+        echo 'expect {'
+        echo '  -re {'$EXP'\r} {puts "OK."; set found true; exp_continue}'
+        echo '   timeout {puts "FAILED!"; exit}'
+        echo '  "'$sstring'>" {if {!$found} {puts "FAILED!"; exit}}'
+        echo '}'
+        echo 
+    else # reject
+        echo 'puts -nonewline "'$state' '$SND' reject '$EXP'... "'
+        echo 'send    "'$SND'\n"'
+        echo 'expect {'
+        echo '  "'$EXP'" {puts          FAILED."; exit}'
+        echo '   timeout {puts "FAILED"; exit}'
+        echo '  "'$sstring'>" {puts "OK."}'
+        echo '}'
+        echo 
+    fi
+
+done
+echo 'puts ""'
+echo 'puts ""'
